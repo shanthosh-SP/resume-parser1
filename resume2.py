c892e3b9c3822a5bfe3d2348bf23314949a4809b
Shanthosh <68721243+shanthosh-SP@users.noreply.github.com>
2023-02-22 15:50:23 +0530

    Update resume2.py

--- resume2.py
+++ resume2.py
@@ -33,7 +33,7 @@ if st.button("Process"):
 
 	Skills_extraction=ResumeParser(filename).get_extracted_data()
 	
-	extract_for_YoE=ResumeParser(filename).get_extracted_data()
+	extract_for_YoE=resumeparse.read_file(filename)
 	#st.write("Name of the Candidate: ",Skills_extraction['name'])
 #st.write("Skills----",Skills_extraction['skills'])
 	#st.write("Years of Experience-----",extract_for_YoE['total_exp'])
@@ -41,6 +41,8 @@ if st.button("Process"):
 	Skills_extracted=Skills_extraction['skills']
 	
 	st.write(extract_for_YoE)
+	st.write(=====)
+	st.write(Skills_extraction)
 	skills_reqd_DS=['machine learning','data mining','predictive modeling', 'statistical analysis', 'data visualization', 'natural language processing', 'big data', 'data warehousing', 'sql', 'python/r programming', 'deep learning', 'artificial intelligence', 'data analytics', 'a/b testing', 'feature engineering', 'etl processes', 'time series analysis', 'regression analysis', 'cluster analysis', 'decision trees','power bi']
 	skills_reqd_HR=['ats','applicant tracking systems','job postings', 'sourcing','source' ,'interviewing skills', 'hiring process', 'job descriptions', 'talent acquisition', 'diversity and inclusion', 'background checks', 'onboarding','hr consulting' ,'recruiting','recruiter','shortlisting','interviewing','end to end recruitment','deadline','reporting','hire','walk-in drives','phone interviewing',' candidate management systems','decisionmaking','management','psychology','monitoring''cms','screening resumes','lateral']
 	skills_reqd_sales=['sales', 'account management', 'client relationship management', 'sales forecasting', 'sales strategy', 'sales negotiations', 'pipeline management', 'territory management', 'customer acquisition', 'sales performance', 'sales reporting','website sales','cilents','metrics','inside sales','strategic content development','presales executives','cold calling','executive',' marketing','business development','crm','market research', 'website sales', 'inside sales','negotiations','customer service']
