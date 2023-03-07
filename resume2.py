a2dfc9ffb4a01564c8d79d869424c1ee39f41aef
Shanthosh <68721243+shanthosh-SP@users.noreply.github.com>
2023-03-07 22:44:19 +0530

    Update resume2.py

--- resume2.py
+++ resume2.py
@@ -160,27 +160,27 @@ if st.button("Process"):
 	
 	if a<=2:
 		folder_path = "categories/fresher/"	
-		file_contents = file_selector.read()
-		file_name = file_selector.name
-		gh.create_file(folder_path, file_name, file_contents)
+		#file_contents = file_selector.read()
+		#file_name = file_selector.name
+		gh.create_file(folder_path, selected_filename, ResumeParser(filename).get_extracted_data())
 		st.write("Resume saved successfully.")
 	if 2<a<=4:
 		folder_path = "categories/Intermediate/"	
-		file_contents = file_selector.read()
-		file_name = file_selector.name
-		gh.create_file(folder_path, file_name, file_contents)
+		#file_contents = file_selector.read()
+		#file_name = file_selector.name
+		gh.create_file(folder_path, selected_filename, ResumeParser(filename).get_extracted_data())
 		st.write("Resume saved successfully.")
 	if 4<a<=10:
 		folder_path = "categories/Senior/"
-		file_contents = file_selector.read()
-		file_name = uploaded_file.name
-		gh.create_file(folder_path, file_name, file_contents)
+		#file_contents = file_selector.read()
+		#file_name = uploaded_file.name
+		gh.create_file(folder_path, selected_filename, ResumeParser(filename).get_extracted_data())
 		st.write("Resume saved successfully.")
 	if a<=11:
 		folder_path = "categories/Advanced/"	
-		file_contents = file_selector.read()
-		file_name = file_selector.name
-		gh.create_file(folder_path, file_name, file_contents)
+		#file_contents = file_selector.read()
+		#file_name = file_selector.name
+		gh.create_file(folder_path, selected_filename, ResumeParser(filename).get_extracted_data())
 		st.write("Resume saved successfully.")
 	
 	with st.form("myform"):
