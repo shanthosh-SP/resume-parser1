f840ab6a52dc3ad0a85e8cbe077a0c7b0edf31c5
Shanthosh <68721243+shanthosh-SP@users.noreply.github.com>
2023-03-07 16:31:13 +0530

    Update resume2.py

--- resume2.py
+++ resume2.py
@@ -8,14 +8,14 @@ import en_core_web_sm
 import pandas as pd
 
 
-# nltk.download('stopwords')
-# nltk.download('punkt')
-# nltk.download('averaged_perceptron_tagger')
-# nltk.download('universal_tagset')
-# nltk.download('maxent_ne_chunker')
-# nltk.download('stopwords')
-# nltk.download('wordnet')
-# nltk.download('brown')
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('universal_tagset')
+nltk.download('maxent_ne_chunker')
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('brown')
 from resume_parser import resumeparse
 from pyresparser import ResumeParser
 
