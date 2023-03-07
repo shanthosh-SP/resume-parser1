607ba3f3f58f9218684f8b17c4659c34a0000fe5
Shanthosh <68721243+shanthosh-SP@users.noreply.github.com>
2023-03-07 16:29:45 +0530

    Update resume2.py

--- resume2.py
+++ resume2.py
@@ -43,7 +43,7 @@ if st.button("Process"):
 	Skills_extracted=Skills_extraction['skills']
 	
 	st.write(extract_for_YoE)
-	st.write(=====)
+	st.write('=====')
 	st.write(Skills_extraction)
 	skills_reqd_DS=['machine learning','data mining','predictive modeling', 'statistical analysis', 'data visualization', 'natural language processing', 'big data', 'data warehousing', 'sql', 'python/r programming', 'deep learning', 'artificial intelligence', 'data analytics', 'a/b testing', 'feature engineering', 'etl processes', 'time series analysis', 'regression analysis', 'cluster analysis', 'decision trees','power bi']
 	skills_reqd_HR=['ats','applicant tracking systems','job postings', 'sourcing','source' ,'interviewing skills', 'hiring process', 'job descriptions', 'talent acquisition', 'diversity and inclusion', 'background checks', 'onboarding','hr consulting' ,'recruiting','recruiter','shortlisting','interviewing','end to end recruitment','deadline','reporting','hire','walk-in drives','phone interviewing',' candidate management systems','decisionmaking','management','psychology','monitoring''cms','screening resumes','lateral']
