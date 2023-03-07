758dd8bfadce772b7e31fe5779de5faf689c7cf3
Shanthosh <68721243+shanthosh-SP@users.noreply.github.com>
2023-03-07 21:46:01 +0530

    Update resume2.py

--- resume2.py
+++ resume2.py
@@ -157,6 +157,7 @@ if st.button("Process"):
 	final(extract_for_YoE['total_exp'],b)
 	with st.form(key="Form :", clear_on_submit = True):
 		Submit = st.form_submit_button(label='Submit')
+from pathlib import Path
         
 	
 	if Submit :
