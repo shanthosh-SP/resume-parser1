6d3f16ac643630312c0ff675c133d9146baf610d
Shanthosh <68721243+shanthosh-SP@users.noreply.github.com>
2023-03-08 11:48:44 +0530

    Update resume2.py

--- resume2.py
+++ resume2.py
@@ -47,17 +47,17 @@ if st.button("Process"):
 	#st.write("Name of the Candidate: ",Skills_extraction['name'])
 #st.write("Skills----",Skills_extraction['skills'])
 	#st.write("Years of Experience-----",extract_for_YoE['total_exp'])
-	st.write(extract_for_YoE)
-	st.write(extract_for_YoE['total_exp'])
-	#if extract_for_YoE['total_exp']==0
-	#	try:
-# 			years_of_experience = re.findall(r'\d+\+?\s+years', job_description)
-# 			if len(years_of_experience)>0:
-# 				extract_for_YoE['total_exp']=int(years_of_experience[0].split()[0].replace('+',''))
-# 		except:
-# 			years_of_experience = re.findall(r'\d\s*years', job_description)
-# 			if len(years_of_experience)>0:
-# 				extract_for_YoE['total_exp']=int(years_of_experience[0].split()[0])
+	st.write(type(extract_for_YoE))
+	st.write(extract_for_YoE["total_exp"])
+	if extract_for_YoE["total_exp"]==0
+		try:
+			years_of_experience = re.findall(r'\d+\+?\s+years', job_description)
+			if len(years_of_experience)>0:
+				extract_for_YoE['total_exp']=int(years_of_experience[0].split()[0].replace('+',''))
+		except:
+			years_of_experience = re.findall(r'\d\s*years', job_description)
+			if len(years_of_experience)>0:
+				extract_for_YoE['total_exp']=int(years_of_experience[0].split()[0])
 	
 	st.write(extract_for_YoE['total_exp'])
 
