32425feb95eaa31d6db3c4ceef47a9e4647f0833
Shanthosh <68721243+shanthosh-SP@users.noreply.github.com>
2023-03-30 16:54:23 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
-mkdir -p ~/.streamlit/
+#!/bin/bash
 
+# Install poppler-utils and pkg-config
+apt-get update
+apt-get install -y poppler-utils pkg-config
+
+# Create Streamlit config file
+mkdir -p ~/.streamlit/
 echo "\
 [server]\n\
 headless = true\n\
@@ -7,6 +13,3 @@ port = $PORT\n\
 enableCORS = false\n\
 \n\
 " > ~/.streamlit/config.toml
-# Install pkg-config
-sudo apt-get update
-sudo apt-get install -y pkg-config
