0ba3f56df65bb4fe0315d08985f28a30a477240a
Shanthosh <68721243+shanthosh-SP@users.noreply.github.com>
2023-03-30 09:28:07 +0530

    Update resume2.py

--- resume2.py
+++ resume2.py
@@ -19,50 +19,73 @@ from resume_parser import resumeparse
 from pyresparser import ResumeParser
 import docx2txt
 
-
-def select_file(folder_path='Resumes'):
+def select_resume_file(folder_path='Resumes'):
     filenames = os.listdir(folder_path)
-    selected_filename = st.selectbox('Select a file', filenames)
+    selected_filename = st.selectbox('Select a resume file', filenames)
     return os.path.join(folder_path, selected_filename)
 
-def extract_skills(filename):
-    if filename.endswith(".pdf"):
-        skills_extracted = ResumeParser(filename).get_extracted_data()['skills']
-        skills_extracted = [skill.lower().strip(' )(') for skill in skills_extracted]
-        return skills_extracted
-    return []
+def extract_skills(resume_file):
+    Skills_extraction = ResumeParser(resume_file).get_extracted_data()
+    extract_for_YoE = resumeparse.read_file(resume_file)
+    Skills_extracted = Skills_extraction['skills']
+    Skills_extracted = [x.lower().strip() for x in Skills_extracted]
+    res = {'Data Scientist': [], 'HR': [], 'Sales': []}
+    for skill in Skills_extracted:
+        if skill in ["machine learning"]:
+            res['Data Scientist'].append(skill)
+        if skill in ["recruiting", "talent acquisition"]:
+            res['HR'].append(skill)
+        if skill in ["sales", "marketing"]:
+            res['Sales'].append(skill)
+    return res, extract_for_YoE['total_exp']
 
-def get_job_role(skills_extracted):
-    skills_reqd_ds = ["machine learning"]
-    skills_reqd_hr = ["recruiting", "talent acquisition"]
-    skills_reqd_sales = ["sales", "marketing"]
-    count_ds = sum([1 for skill in skills_extracted if skill in skills_reqd_ds])
-    count_hr = sum([1 for skill in skills_extracted if skill in skills_reqd_hr])
-    count_sales = sum([1 for skill in skills_extracted if skill in skills_reqd_sales])
-    if count_ds > count_hr and count_ds > count_sales:
-        return "Data Scientist"
-    elif count_hr > count_ds and count_hr > count_sales:
+def predict_job_role(skills):
+    if skills['HR'] > skills['Data Scientist'] and skills['HR'] > skills['Sales']:
         return "HR"
-    elif count_sales > count_ds and count_sales > count_hr:
+    elif skills['Data Scientist'] > skills['HR'] and skills['Data Scientist'] > skills['Sales']:
+        return "Data Scientist"
+    elif skills['Sales'] > skills['HR'] and skills['Sales'] > skills['Data Scientist']:
         return "Sales"
-    return ""
+    else:
+        return "Undetermined"
 
-def display_salary_data(total_exp, job_role):
-    salary_data = pd.read_csv("Sal_data.csv")
-    salary_data = salary_data.loc[salary_data['Job Role'] == job_role]
-    if total_exp <= 2:
-        salary_data = salary_data.loc[salary_data['YoE'] == "0-2"]
-    elif total_exp >= 11:
-        salary_data = salary_data.loc[salary_data['YoE'] == "10+"]
+def select_salary_data():
+    sal_data = pd.read_csv("Sal_data.csv")
+    return sal_data
+
+def get_salary_for_experience(sal_data, job_role, experience):
+    if experience <= 2:
+        yoe = "0-2"
+    elif experience >= 11:
+        yoe = "10+"
+    else:
+        yoe = str(experience) + "-" + str(experience + 1)
+    return sal_data.loc[(sal_data['Job Role'] == job_role) & (sal_data['YoE'] == yoe)]
+
+def display_salary_prediction(sal_data, job_role, experience):
+    salary_data = get_salary_for_experience(sal_data, job_role, experience)
+    st.write("Salary prediction:")
     st.write(salary_data)
 
-filename = select_file()
+def process_resume_file(resume_file):
+    if resume_file.endswith(".pdf"):
+        skills, experience = extract_skills(resume_file)
+        job_role = predict_job_role(skills)
+        sal_data = select_salary_data()
+        display_salary_prediction(sal_data, job_role, experience)
+        st.write(f"He is specialised in {job_role}")
+    elif resume_file.endswith(".doc") or resume_file.endswith(".docx"):
+        text = docx2txt.process(resume_file)
+        pattern = r"(\d+)\+?\s*(years?|yrs)"
+        matches = re.findall(pattern, text, re.IGNORECASE)
+        experience = int(matches[0][0]) if matches else 0
+        st.write(f"Years of experience in {resume_file}: {experience}")
+        skills, _ = extract_skills(resume_file)
+        st.write("The skills that get matched with our keywords:", skills)
+        st.write("Job role prediction: Undetermined")
+
+# main code
+resume_file = select_resume_file()
 if st.button("Process"):
-    skills_extracted = extract_skills(filename)
-    job_role = get_job_role(skills_extracted)
-    if job_role:
-        st.write(f"He is specialized in {job_role}")
-        total_exp = resumeparse.read_file(filename)['total_exp']
-        display_salary_data(total_exp, job_role)
-    else:
-        st.write("No suitable job role found based on the skills extracted.")
+    process_resume_file(resume_file)
+
